d18add927cbd74fb771241528e108aa8bcc8bf54
avi3s <avirup.pal@gmail.com>
2022-08-23 11:19:55 +0530

    Initial Commit

---
+++ src/main/java/com/eauction/configserver/EAuctionConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.eauction.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAuctionConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAuctionConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eauction/configserver/EAuctionConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eauction.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAuctionConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
